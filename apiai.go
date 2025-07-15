10b07d5bc7557fecf6e608777de514072fcacef4
ZANYRI <zanyri880@gmail.com>
2025-07-15 14:09:12 +0300

    connect ai-service

---
+++ apiai.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
+)
+
+func ReqApi(message string) (string, error) {
+
+	config, err := ReadEnv()
+
+	if err != nil {
+		log.Fatal("Ошибка!")
+	} 
+
+	client := openai.NewClient(
+	option.WithAPIKey(config.ApiKey),
+	option.WithBaseURL("https://openrouter.ai/api/v1"),
+	)
+	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage(message),
+		},
+		Model: "moonshotai/kimi-k2:free",
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return chatCompletion.Choices[0].Message.Content, nil
+
+}
